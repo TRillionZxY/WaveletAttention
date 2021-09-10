501b894c1a1d4882006a7c529faca8c95261e18b
ZHAO <iezhaoxy@163.com>
2021-09-10 12:45:57 +0800

    update

--- models/attentions/__init__.py
+++ models/attentions/__init__.py
@@ -2,5 +2,7 @@ from .. import find_module_using_name
 
 
 def get_attention_module(attention_type="none"):
-
-    return find_module_using_name(attention_type.lower())
+    if attention_type == "none":
+        return None
+    else:    
+        return find_module_using_name(attention_type.lower())
