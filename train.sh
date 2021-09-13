119bca9652abcac6efa13d3ede7f56e545fdb911
ZHAO <iezhaoxy@163.com>
2021-09-13 20:52:13 +0800

    update

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@ tmpFolder="~/data"
 # Training Baseline from scratch
 python cifar_main.py --dataset cifar10 --dataset_dir ${tmpFolder} --block_type basic --batch_size 256 --arch resnet18
 
-# # Training Attention from scratch
-# python cifar_main.py --dataset cifar10 --dataset_dir ${tmpFolder} --arch resnet18 --attention_type cbam
+# Training Attention from scratch
+#python cifar_main.py --dataset cifar10 --dataset_dir ${tmpFolder} --block_type basic --batch_size 256 --arch resnet18 --attention_type cbam
 
-# # Training WA from scratch
-# python cifar_main.py --dataset cifar10 --dataset_dir ${tmpFolder} --arch resnet18 --attention_type wa --attention_param haar
+# Training WA from scratch
+#python cifar_main.py --dataset cifar10 --dataset_dir ${tmpFolder} --block_type basic --batch_size 256 --arch resnet18 --attention_type wa --attention_param haar
