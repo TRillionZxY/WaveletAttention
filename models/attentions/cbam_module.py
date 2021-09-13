bd081c3a1212db60bc3e554ef93b34fe546fed9f
ZHAO <iezhaoxy@163.com>
2021-09-13 20:51:04 +0800

    update

--- models/attentions/cbam_module.py
+++ models/attentions/cbam_module.py
@@ -56,7 +56,7 @@ class ChannelGate(nn.Module):
             else:
                 channel_att_sum = channel_att_sum + channel_att_raw
 
-        scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+        scale = torch.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
         return x * scale
 
 def logsumexp_2d(tensor):
