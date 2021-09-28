38c71c7d1cc3914e8efe2303c64bed8204c7f3b9
ZHAO <iezhaoxy@163.com>
2021-09-28 15:20:49 +0800

    update wa

--- models/attentions/wa_module.py
+++ models/attentions/wa_module.py
@@ -7,14 +7,15 @@ class wa_module(nn.Module):
     def __init__(self, wavename='haar'):
         super(wa_module, self).__init__()
         self.dwt = DWT_2D(wavename=wavename)
-        
+        self.F = torch.add()
+        self.softmax = nn.Softmax2d()
+        self.delta = torch.mul()
+
     @staticmethod
     def get_module_name():
         return "wa"
 
     def forward(self, input):
         LL, LH, HL, _ = self.dwt(input)
-        output = torch.add(LH, HL).cuda()
-        output = torch.Tensor(nn.Softmax(output)).cuda()
-        output = LL + torch.mul(LL, output).cuda()
+        output = self.F(LL, self.delta(LL, self.softmax(self.F(LH, HL))))
         return output, LL
