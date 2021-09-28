30c6ba80c5042806eae2929e136c6b3908d15da9
ZHAO <iezhaoxy@163.com>
2021-09-28 15:57:12 +0800

    update

--- models/attentions/wa_module.py
+++ models/attentions/wa_module.py
@@ -8,7 +8,7 @@ from DWT import DWT_2D
 class wa_module(nn.Module):
     def __init__(self, wavename='haar'):
         super(wa_module, self).__init__()
-        self.dwt = DWT_2D(wavename=wavename)
+        self.dwt = DWT_2D(wavename=wavename).cuda()
         self.softmax = nn.Softmax2d()
 
     @staticmethod
